fd200e8be71b064e8cdcc6467c9ef5ab01ea0a16
keita.midorikawa <keita.midorikawa@beex-inc.com>
2021-12-28 15:09:02 +0900

    ecrテンプレート作成

--- app.py
+++ app.py
@@ -5,12 +5,16 @@ from aws_cdk import core
 from cdk_template.context.sample_stage_context import SampleStageContextLoader
 from cdk_template.stack.sample_stage_stack import SampleStageStack
 from cdk_template.context.sample_line_context import SampleLineContextLoader
-from cdk_template.stack.sample_line_stack import LineStackBase, SampleLineStack
+from cdk_template.stack.sample_line_stack import SampleLineStack
+from cdk_template.context.ecr_context import EcrContextLoader
+from cdk_template.stack.ecr_stack import EcrStack
 
 KEY_CONTEXT_DEFAULT = "default"
 KEY_CONTEXT_OVERWRITE = "overwrite"
 ARG_KEY_CONTEXT_STAGE = "stage"
 ARG_KEY_CONTEXT_LINE = "line"
+VPC_ENDPOINT_ECR_API = "vpce-0655dc8212300bc9f"
+VPC_ENDPOINT_ECR_DKR = "vpce-087f93b6522676ab5"
 
 app = core.App()
 default_context = app.node.try_get_context(KEY_CONTEXT_DEFAULT)
@@ -24,6 +28,13 @@ if stage:
         stage=stage
     ).load()
     SampleStageStack(app, sample_stage_context)
+
+    ecr_context = EcrContextLoader(
+        default_context=default_context,
+        overwrite_context=overwrite_context,
+        stage=stage
+    ).load()
+    EcrStack(app, ecr_context)
 
 
     line = app.node.try_get_context(ARG_KEY_CONTEXT_LINE)
---
+++ cdk_template/stack/ecr_stack.py
@@ -0,0 +1,72 @@
+import json
+from aws_cdk import (
+    aws_ecr as ecr
+)
+from cdk_template.context.ecr_context import EcrContext
+from cdk_template.stack.stack_base import StageStackBase
+
+class EcrStack(StageStackBase):
+    STACK_NAME = "ecr"
+    context: EcrContext
+
+    def _ecr_repositories(self, repository_name: str):
+        lifecycle_policy = json.dumps(
+            {
+                "rules": [
+                    {
+                        "action": {
+                            "type": "expire",
+                        },
+                        "selection": {
+                            "countType": "imageCountMoreThan",
+                            "countNumber": 10,
+                            "tagStatus": "untagged",
+                        },
+                        "description": "delete untagged image cycle",
+                        "rulePriority": 1,
+                    }
+                ],
+            }
+        )
+
+        repository_policy = {
+            "Version": "2012-10-17",
+            "Statement": [
+                {
+                    "Sid": "Deny",
+                    "Effect": "Deny",
+                    "Principal": "*",
+                    "Action": ["ecr:*"],
+                    "Condition": {
+                        "StringNotEquals": {
+                            "aws:CalledVia": [
+                                "cloudformation.amazonaws.com",
+                            ],
+                            "aws:sourceVpce": self.context.allow_vpce_list,
+                        },
+                        "NotIpAddress": {
+                            "aws:SourceIp": self.context.allow_ip_list,
+                        },
+                    },
+                }
+            ],
+        }
+
+        ecr_repo = ecr.CfnRepository(
+            self, self._get_resource_id(f"ecr_{repository_name}"),
+            repository_name=self._get_full_repository_name(repository_name),
+            image_scanning_configuration=ecr.CfnRepository.ImageScanningConfigurationProperty(
+                scan_on_push=True,
+            ),
+            repository_policy_text=repository_policy,
+            lifecycle_policy=ecr.CfnRepository.LifecyclePolicyProperty(
+                lifecycle_policy_text=lifecycle_policy,
+            ),
+        )
+
+    def _get_full_repository_name(self, repository_name: str) -> str:
+        return f"{self.context.app_name}-{self.context.stage}/{repository_name}"
+
+    def _resources(self):
+        for repository_name in self.context.ecr_repositories:
+            self._ecr_repositories(repository_name)
